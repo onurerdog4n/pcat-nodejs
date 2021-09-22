f5f667c65002075b51c87c1c54eaa39c47768411
onurerdog4n <79117426+onurerdog4n@users.noreply.github.com>
2021-09-22 15:07:36 +0300

    package-lock.json
    Temp and static files

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// MiddleWares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //   const photo = {
+  //     id: 1,
+  //     name: 'Photo Name',
+  //     description: 'Photo description',
+  //   };
+  //   res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
